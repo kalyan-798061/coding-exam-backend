84e9de1b87492847cbc7640854c5a5a1ea0ebaba
kalyan-798061 <kalyanseekoti@gmail.com>
2026-03-09 12:29:29 +0530

    update_env_key

--- project_coding/settings.py
+++ project_coding/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'c1il490f*=yjqug1zk2_*!7-q0$z4k^o94)untsk^iv07&)2ch'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -96,11 +96,14 @@ WSGI_APPLICATION = 'project_coding.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+import os
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600
+    )
 }
 
 
